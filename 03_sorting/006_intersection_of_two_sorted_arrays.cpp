d239013055fc93b2a1aba116fa0cd3d9d9d30e5d
Rocketman <abhinayshambharkar1@gmail.com>
2026-01-28 17:30:33 +0530

    sorting push 2

---
+++ 03_sorting/006_intersection_of_two_sorted_arrays.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void intersection(int a[], int m, int b[], int n){
+	int i=0,j=0;
+	while(i<m && j<n){
+		if(i>0 && a[i]==a[i-1]){
+			i++;
+			continue;
+		}
+		if(j>0 && b[j]==b[j-1]){
+			j++;
+			continue;
+		}
+		if(a[i]<b[j]) i++;
+		else if(a[i]>b[j])j++;
+		else{
+			cout<<a[i]<<" ";
+			i++;
+			j++;
+		}
+	}
+}
+
+int main(){
+    int a[]={3,5,10,10,15,15,20};
+    int b[]={5,10,10,15,30};
+    int m = sizeof(a)/sizeof(a[0]);
+    int n = sizeof(b)/sizeof(b[0]);
+    intersection(a,m,b,n);
+    return 0;
+}
